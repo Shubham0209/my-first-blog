a6351f81b715fd6fda45f683e4b1429385b223bf
shubham <shubhamp.singh@yahoo.com>
2018-09-21 23:03:43 +0530

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    #int:pk>  It means that Django expects an integer value and will transfer it to a view as a variable called pk.
+    #if you enter http://127.0.0.1:8000/post/5/ into your browser, Django will understand that you are looking for a view 
+    #called post_detail and transfer the information that pk equals 5 to that view.
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.utils import timezone
 from .models import Post #The dot before models means current directory or current application.
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk) # In case there is no Post with the given pk, it will display much nicer page, the Page Not Found 404 page
+    return render(request, 'blog/post_detail.html', {'post': post})
